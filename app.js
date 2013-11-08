e24e14f0b3b4b2cf316a7c37f8219378940a23de
Mike Meier <mike.meier@ibrows.ch>
2013-11-08 21:00:57 +0100

    serve index.html over http server

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-var io = require('socket.io').listen(3000);
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
 var randomstring = require('randomstring');
+var fs = require('fs');
+
+app.listen(8080);
+
+function handler(req, res){
+    fs.readFile(__dirname + '/index.html', function(err, data){
+        if(err){
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+};
 
 io.sockets.on('connection', function(socket){
     var id = randomstring.generate();
